8b8c22e5a8facc8b878ea4691f96b733f62676a6
Keefe Liu <keefe.l@users.noreply.github.com>
2023-12-14 18:57:31 +0800

    update bundler test

--- e2e/bundler_test.go
+++ e2e/bundler_test.go
@@ -2,6 +2,7 @@ package e2e
 
 import (
 	"context"
+	"cosmossdk.io/math"
 	"fmt"
 	"gorm.io/gorm"
 	"os"
@@ -10,7 +11,10 @@ import (
 	"time"
 
 	"github.com/bnb-chain/greenfield-go-sdk/client"
+	"github.com/bnb-chain/greenfield-go-sdk/pkg/utils"
 	"github.com/bnb-chain/greenfield-go-sdk/types"
+	types2 "github.com/bnb-chain/greenfield/sdk/types"
+	permTypes "github.com/bnb-chain/greenfield/x/permission/types"
 	storageTypes "github.com/bnb-chain/greenfield/x/storage/types"
 
 	"github.com/node-real/greenfield-bundle-service/bundler"
@@ -19,72 +23,139 @@ import (
 	"github.com/node-real/greenfield-bundle-service/util"
 )
 
-var (
-	objectContent = "Hello, Greenfield Bundle! Welcome to test and use it!"
+const (
+	ownerPrivateKey = "" // Set your own private key for testing
+	bucketName      = "gnfd-bundler-test"
+	objectContent   = "Hello, Greenfield Bundle! Welcome to test and use it!"
 )
 
-func prepareBundleAndObject(db *gorm.DB, config *util.ServerConfig, client client.IClient, account *types.Account) (string, string, error) {
-	ctx := context.Background()
-	accInfo, err := client.GetAccount(context.Background(), account.GetAddress().String())
+var (
+	ownerAcc   *types.Account
+	bundlerAcc *types.Account
+	gnfdClient client.IClient
+)
+
+func setupAccounts(config *util.ServerConfig) {
+	var err error
+
+	ownerAcc, err = types.NewAccountFromPrivateKey("owner-account", ownerPrivateKey)
 	if err != nil {
-		return "", "", fmt.Errorf("get account failed: %v", err)
+		util.Logger.Fatalf("create owner account failed, err=%v", err.Error())
 	}
 
-	bucketName := "bundler-test"
-	bundleName := fmt.Sprintf("bundle-%d", accInfo.GetSequence())
-	objectName := "object1.txt"
-
-	_, err = client.HeadBucket(ctx, bucketName)
+	bundlerAcc, err = types.NewAccountFromPrivateKey("bundler-account", config.BundleConfig.BundlerPrivateKeys[0])
 	if err != nil {
-		_, err = client.CreateBucket(ctx, bucketName, "0xbb0cac4668d970db8b51b6c1f80e6627f2bd3c02", types.CreateBucketOptions{Visibility: storageTypes.VISIBILITY_TYPE_PUBLIC_READ})
-		if err != nil {
-			return bucketName, bundleName, fmt.Errorf("create bucket %s failed: %v", bucketName, err)
+		util.Logger.Fatalf("create bundler account failed, err=%v", err.Error())
+	}
+
+	gnfdClient, err = client.New(config.GnfdConfig.ChainId, config.GnfdConfig.RpcUrl, client.Option{DefaultAccount: ownerAcc})
+	if err != nil {
+		util.Logger.Fatalf("create greenfield client failed, account=%s, err=%v", ownerAcc.GetAddress().String(), err.Error())
+	}
+}
+
+func setupBucket() {
+	ctx := context.Background()
+	_, err := gnfdClient.HeadBucket(ctx, bucketName)
+	if err == nil {
+		return
+	}
+
+	_, err = gnfdClient.CreateBucket(ctx, bucketName, "0xbb0cac4668d970db8b51b6c1f80e6627f2bd3c02", types.CreateBucketOptions{Visibility: storageTypes.VISIBILITY_TYPE_PUBLIC_READ})
+	if err != nil {
+		util.Logger.Fatalf("create bucket %s failed: %v", bucketName, err)
+	}
+}
+
+func grantFeesAndPermission() {
+	ctx := context.Background()
+
+	bucketActions := []permTypes.ActionType{permTypes.ACTION_CREATE_OBJECT}
+	statements := utils.NewStatement(bucketActions, permTypes.EFFECT_ALLOW, nil, types.NewStatementOptions{})
+	principal, err := utils.NewPrincipalWithAccount(bundlerAcc.GetAddress())
+	if err != nil {
+		util.Logger.Fatalf("fail to generate marshaled principal: %v", err)
+	}
+	txHash, err := gnfdClient.PutBucketPolicy(ctx, bucketName, principal, []*permTypes.Statement{&statements}, types.PutPolicyOption{})
+	if err != nil {
+		util.Logger.Fatalf("put policy failed: %v", err)
+	}
+	_, err = gnfdClient.WaitForTx(ctx, txHash)
+	if err != nil {
+		util.Logger.Fatalf("wait for grant permission tx failed: %v", err)
+	}
+
+	allowanceAmount := math.NewIntWithDecimal(1, 18)
+	allowance, err := gnfdClient.QueryBasicAllowance(ctx, ownerAcc.GetAddress().String(), bundlerAcc.GetAddress().String())
+	if err == nil {
+		for _, coin := range allowance.SpendLimit {
+			if coin.Denom == types2.Denom && coin.Amount.GTE(allowanceAmount) {
+				return
+			}
 		}
 	}
 
+	txHash, err = gnfdClient.GrantBasicAllowance(ctx, bundlerAcc.GetAddress().String(), allowanceAmount, nil, types2.TxOption{})
+	if err != nil {
+		util.Logger.Fatalf("grant fee allowance failed: %v", err)
+	}
+	_, err = gnfdClient.WaitForTx(ctx, txHash)
+	if err != nil {
+		util.Logger.Fatalf("wait for grant fee tx failed: %v", err)
+	}
+}
+
+func setupDatabaseRecords(db *gorm.DB, config *util.ServerConfig) string {
+	ctx := context.Background()
+	accInfo, err := gnfdClient.GetAccount(ctx, ownerAcc.GetAddress().String())
+	if err != nil {
+		util.Logger.Fatalf("get owner account failed: %v", err)
+	}
+
+	bundleName := fmt.Sprintf("bundle-%d", accInfo.GetSequence())
+	objectName := "test-object.txt"
 	bundle := database.Bundle{
-		Owner:           account.GetAddress().String(),
+		Owner:           ownerAcc.GetAddress().String(),
 		Bucket:          bucketName,
 		Name:            bundleName,
-		BundlerAccount:  account.GetAddress().String(),
+		BundlerAccount:  bundlerAcc.GetAddress().String(),
 		Status:          database.BundleStatusBundling,
 		MaxFiles:        100,
 		MaxSize:         1024 * 1024 * 1024,
 		MaxFinalizeTime: 10, // Seconds
 	}
-
 	result := db.Create(&bundle)
 	if result.Error != nil {
-		return bucketName, bundleName, fmt.Errorf("create bundle error, err=%s", result.Error.Error())
+		util.Logger.Fatalf("create bundle error, err=%s", result.Error.Error())
 	}
 
 	objectPath := storage.GetObjectPath(config.BundleConfig.LocalStoragePath, bucketName, bundleName, objectName)
-	if err := os.MkdirAll(filepath.Dir(objectPath), os.ModePerm); err != nil {
-		return bucketName, bundleName, fmt.Errorf("mkdir for object %s failed: %v", objectPath, err)
+	err = os.MkdirAll(filepath.Dir(objectPath), os.ModePerm)
+	if err != nil {
+		util.Logger.Fatalf("mkdir for object %s failed: %v", objectPath, err)
 	}
 	objectFile, err := os.Create(objectPath)
 	if err != nil {
-		return bucketName, bundleName, fmt.Errorf("create object %s failed: %v", objectPath, err)
+		util.Logger.Fatalf("create object %s failed: %v", objectPath, err)
 	}
 	size, err := objectFile.Write([]byte(objectContent))
 	if err != nil {
-		return bucketName, bundleName, fmt.Errorf("write object %s failed: %v", objectPath, err)
+		util.Logger.Fatalf("write object %s failed: %v", objectPath, err)
 	}
 
 	object := database.Object{
 		Bucket:     bucketName,
 		BundleName: bundleName,
 		ObjectName: objectName,
-		Owner:      account.GetAddress().String(),
+		Owner:      ownerAcc.GetAddress().String(),
 		Size:       int64(size),
 	}
-
 	result = db.Create(&object)
 	if result.Error != nil {
-		return bucketName, bundleName, fmt.Errorf("create object error, err=%s", result.Error.Error())
+		util.Logger.Fatalf("create object error, err=%s", result.Error.Error())
 	}
 
-	return bucketName, bundleName, nil
+	return bundleName
 }
 
 func TestBundler(t *testing.T) {
@@ -101,20 +172,10 @@ func TestBundler(t *testing.T) {
 		util.Logger.Fatalf("no bundler account available")
 	}
 
-	account, err := types.NewAccountFromPrivateKey("bundler-account", config.BundleConfig.BundlerPrivateKeys[0])
-	if err != nil {
-		util.Logger.Fatalf("create bundler account failed, err=%v", err.Error())
-	}
-
-	client, err := client.New(config.GnfdConfig.ChainId, config.GnfdConfig.RpcUrl, client.Option{DefaultAccount: account})
-	if err != nil {
-		util.Logger.Fatalf("create greenfield client failed, account=%s, err=%v", account.GetAddress().String(), err.Error())
-	}
-
-	bucketName, bundleName, err := prepareBundleAndObject(db, config, client, account)
-	if err != nil {
-		util.Logger.Fatalf("prepare failed, err=%v", err.Error())
-	}
+	setupAccounts(config)
+	setupBucket()
+	grantFeesAndPermission()
+	bundleName := setupDatabaseRecords(db, config)
 
 	bundler, err := bundler.NewBundler(config, db)
 	go bundler.Run()
@@ -124,17 +185,19 @@ func TestBundler(t *testing.T) {
 	for {
 		select {
 		case <-ticker.C:
-			objDetails, err := client.HeadObject(context.Background(), bucketName, bundleName)
-			if err == nil {
-				util.Logger.Infof("bundle on-chain success, name=%s, status=%v", objDetails.ObjectInfo.ObjectName, objDetails.ObjectInfo.ObjectStatus)
-				if objDetails.ObjectInfo.ObjectStatus == storageTypes.OBJECT_STATUS_SEALED {
-					_, _, err := client.GetObject(context.Background(), bucketName, bundleName, types.GetObjectOptions{})
-					if err != nil {
-						util.Logger.Fatalf("get bundled object failed, err=%v", err.Error())
-					}
+			objDetails, err := gnfdClient.HeadObject(context.Background(), bucketName, bundleName)
+			if err != nil {
+				continue
+			}
 
-					return
+			util.Logger.Infof("bundle on-chain success, name=%s, status=%v", objDetails.ObjectInfo.ObjectName, objDetails.ObjectInfo.ObjectStatus)
+			if objDetails.ObjectInfo.ObjectStatus == storageTypes.OBJECT_STATUS_SEALED {
+				_, _, err := gnfdClient.GetObject(context.Background(), bucketName, bundleName, types.GetObjectOptions{})
+				if err != nil {
+					util.Logger.Fatalf("get bundled object failed, err=%v", err.Error())
 				}
+
+				return
 			}
 		}
 	}
